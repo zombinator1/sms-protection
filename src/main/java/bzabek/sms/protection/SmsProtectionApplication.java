4ca764d99f6bae8cdd5d222b3741204c69cb989b
Bartek <bzabekk@gmail.com>
2025-05-08 12:33:17 +0200

    Initial commit

---
+++ src/main/java/bzabek/sms/protection/SmsProtectionApplication.java
@@ -0,0 +1,13 @@
+package bzabek.sms.protection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsProtectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmsProtectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bzabek/sms/protection/SmsProtectionApplicationTests.java
@@ -0,0 +1,13 @@
+package bzabek.sms.protection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsProtectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
